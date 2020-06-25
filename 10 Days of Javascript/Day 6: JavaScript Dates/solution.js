5669f49e6a6bcbc24281084d6a7b879c5291bab7
pleximaDev <41956118+pleximaDev@users.noreply.github.com>
2020-06-25 16:21:04 +0300

    Create solution.js

---
+++ 10 Days of Javascript/Day 6: JavaScript Dates/solution.js
@@ -0,0 +1,43 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function getDayName(dateString) {
+    let dayName = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"), 
+    monthName = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
+    date = dateString.split("/", 3), tempDate = String(monthName[Number(date[0]) - 1] + " " + date[1] + ", " + date[2]), 
+    dateTemp = new Date(tempDate);
+    // console.log(monthName[Number(date[1])], date[0], ", ", date[2]);
+    return dayName[dateTemp.getDay()];
+}
+
+
+function main() {
+    const d = +(readLine());
+    
+    for (let i = 0; i < d; i++) {
+        const date = readLine();
+        
+        console.log(getDayName(date));
+    }
+}
